f839f0657e3ba88bd417112fed49944f93e64f69
Suyan Man Amatya <srkamatya007@gmail.com>
2024-05-17 08:20:35 +0545

    backend add

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -6,28 +6,48 @@ import Architect from "./components/departments/Architect";
 import Faculties from "./components/faculty/Faculties";
 import Home from "./components/home/Home";
 import Navbar from "./components/navbar/Navbar";
+import Form from "./components/form/Form";
+
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useState } from "react";
 function App() {
-  return (
-    <>
-      <div className="mx-28">
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/faculty/Faculties" element={<Faculties />} />
-            <Route exact path="/departments/Civil" element={<Civil />} />
-            <Route exact path="/departments/Computer" element={<Computer />} />
-            <Route
-              exact
-              path="/departments/Architect"
-              element={<Architect />}
-            />
-          </Routes>
-        </Router>
-      </div>
-    </>
-  );
+  const [verify, setVerify] = useState(false);
+
+  if (verify === true) {
+    return (
+      <>
+        <PreLoader />
+        <div className="mx-28">
+          <Router>
+            <Navbar />
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/faculty/Faculties" element={<Faculties />} />
+              <Route exact path="/departments/Civil" element={<Civil />} />
+              <Route
+                exact
+                path="/departments/Computer"
+                element={<Computer />}
+              />
+              <Route
+                exact
+                path="/departments/Architect"
+                element={<Architect />}
+              />
+            </Routes>
+          </Router>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <Form
+        props={() => {
+          setVerify(true);
+        }}
+      />
+    );
+  }
 }
 
 export default App;
